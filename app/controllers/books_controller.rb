6fd3e328e6f43eac20f321aae39abd2380f71cf1
青島優 <aoshima.yu.0402@gmail.com>
2020-09-17 20:32:37 +0900

    firstcommit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :index, :show, :edit]
+
+  def show
+    @book_show = Book.find(params[:id])
+    @user = @book_show.user
+    @book = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if current_user.id == @book.user_id
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+    @book.destoy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
